0e73195b94accd73759927d2071596b536232307
fersin <79020501+fersin@users.noreply.github.com>
2022-12-04 22:24:32 -0500

    Update luis_jaramillo-hardening.sh

--- luis_jaramillo-hardening.sh
+++ luis_jaramillo-hardening.sh
@@ -4,11 +4,17 @@ system=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 
 if [[ $system == *"Centos"* ]];
 then
-    echo "Your OS is centos"
+        echo "Your OS is centos"
+        echo "installing epel repositories." yum install epel-release
+        echo cho "updating and upgrading the server..." yum update -y  && yum upgrade -y 
+        echo "installing clamav antivirus..." yum install clamav
+
 fi
 
-if [[ $system == *"Kali"* ]];
+if [[ $system == *"Ubuntu"* ]];
 then
-    echo "Your OS is Ubuntu"
+        echo "Your OS is Ubuntu"
+        echo "updating and upgrading the server..." yum update -y  && yum upgrade -y
+        echo "installing clamav antivirus..." yum install clamav
 fi
 
